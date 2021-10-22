20067f5e11942d68d6ae1512119c05d517605e91
rayane-local <mrrayanelamri@gmail.com>
2021-10-22 15:42:50 +0200

    database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url('postgres://wthmtpwkvuoxop:ad8381bac272b5a921ea986cce2a23901d50ac8b26195aaefb17a5acf6d904f8@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d8armjdft10cgd');
 
 return [
 
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
